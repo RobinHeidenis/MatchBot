7aae912240b37d566c56e95db08fa827bac87726
Julien <jzapata@live.nl>
2021-05-21 17:19:31 +0200

    feat: add prefix and command/args support

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '!';
 
 client.login(process.env.TOKEN);
 
@@ -9,26 +10,31 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    const args: string[] = msg.content.slice(prefix.length).trim().split(/ +/);
+    const command: string = args.shift().toLowerCase();
+
+    if (command === 'ping') {
         msg.reply('Pong!');
     }
-    if (msg.content === 'profile') {
+    if (command === 'profile') {
         //TODO: fetch user profile from db, reply it to the user.
         //TODO: if the user doesn't have a profile yet, ask to set it up.
         msg.reply('This feature has not been implemented yet. This would normally show your profile');
     }
-    if (msg.content === 'match') {
+    if (command === 'match') {
         //TODO: fetch user profile from db, compare it to other profiles, return the best one's username to the user.
         //TODO: if this user profile has already been suggested, ignore it and pick the next best.
         //TODO: if the user doesn't have a profile yet, ask to set it up.
         msg.reply('This feature has not been implemented yet. This would normally send you a discord users tag');
     }
-    if (msg.content === 'setup') {
+    if (command === 'setup') {
         //TODO: send questions one by one, save the answer to the questions in the db.
         //TODO: once finished, ask the user if they want to find a match now.
         msg.reply('This feature has not been implemented yet. This would normally ask you questions to set up your profile');
     }
-    if (msg.content.startsWith('report')) {
+    if (command.startsWith('report')) {
         let user = msg.content.slice(7);
         if(user) {
             //TODO: add the reporter and reported to db.
