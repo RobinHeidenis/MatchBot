45d7d6edf7aadb8efd2f33cdc118c4874cbcd3fa
Julien <jzapata@live.nl>
2021-05-22 11:27:36 +0200

    feat: match users by finding matching categories

--- match-functions.ts
+++ match-functions.ts
@@ -108,13 +108,30 @@ async function findMatch(message: Message) {
         return await message.reply('Ik heb niemand in mijn lijst met mensen staan 😭.');
     }
 
-    const match = matches[Math.floor(Math.random() * matches.length)];
+    const sentMsg = await message.author.send('Bezig met zoeken...');
+    await sleep(1500); // fake delay
 
-    const sentMsg = await message.author.send('Bezig met zoeken..');
-    await sleep(1500);
-    return await sentMsg.edit(
-        `Match gevonden! Jij en <@${match}> zijn een goede match voor elkaar! Stuur ze een bericht :)`
+    const match = matchUser(user, users);
+    if (!match) return await sentMsg.edit('Ik heb helaas geen match voor je kunnen vinden ☹');
+
+    await sentMsg.edit(
+        `Match gevonden! Jij en <@${match.id}> zijn een goede match voor elkaar! Jullie hebben ${match.matchCount} dingen in common. Stuur ze een bericht :)`
     );
+
+    // TODO: DM the match to let them know they should expect a message soon
+}
+
+/** Calculates the amount of matching categories for each user and sorts them descending
+ * @returns The user with the highest amount of matching categories */
+function matchUser({ categories }: UserData, userPool: UserData[]): UserData & { matchCount: number } {
+    const matches = userPool
+        .map((potentialMatch) => ({
+            ...potentialMatch,
+            matchCount: matchingElements(categories, potentialMatch.categories),
+        }))
+        .sort((a, b) => b.matchCount - a.matchCount);
+    // TODO: if this user profile has already been suggested, ignore it and pick the next best.
+    return matches[0];
 }
 
 async function privacy(message: Message) {
--- utils.ts
+++ utils.ts
@@ -3,3 +3,7 @@ export async function sleep(ms: number) {
         setTimeout(resolve, ms);
     });
 }
+
+export function matchingElements(a1: any[], a2: any[]): number {
+    return a1.filter((v) => a2.includes(v)).length;
+}
