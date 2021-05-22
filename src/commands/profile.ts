b5ce88081db53f555f8685aa51b70917a56de5fd
Julien <jzapata@live.nl>
2021-05-22 18:10:44 +0200

    feat: display the amount of matches on a users profile

--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -9,22 +9,26 @@ export = {
         if (!userExists(message.author)) {
             return await promptRegistration(message);
         }
-        const user = getUser(message.author.id);
+        const user = getUser(message.author.id)!;
 
         const embed = createEmbed(
             'Jouw profiel',
             'De informatie die je tijdens je registratie hebt ingevoerd.',
             {
                 name: 'Categorieën',
-                value: user?.categories?.map((categoryId) => categories[categoryId]).join(', ') ?? '\u200b',
+                value: user.categories?.map((categoryId) => categories[categoryId]).join(', ') ?? '\u200b',
             },
             {
                 name: 'Hobbies',
-                value: user?.hobbies ?? '\u200b',
+                value: user.hobbies ?? '\u200b',
             },
             {
                 name: 'Onderwerpen waar je graag over praat',
-                value: user?.topics ?? '\u200b',
+                value: user.topics ?? '\u200b',
+            },
+            {
+                name: 'Ontmoetingen',
+                value: `Je hebt ${user.matches.length} mensen leren kennen`,
             }
         );
         return message.author.send(embed);
