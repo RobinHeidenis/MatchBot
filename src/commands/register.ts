dd7b50a684bc7b9f0fcf6b1289d9d1cddcd135b3
Julien <jzapata@live.nl>
2021-06-12 10:42:54 +0200

    fix: use bot accounts username rather than a hardcoded one

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -27,9 +27,7 @@ async function startRegistration(message: Message) {
         );
     }
     await message.author.send(
-        'Hi! ik ben LonelinessFighter en ik ben hier om jou met een andere persoon te matchen die dezelfde interesses als jij heeft.' +
-            ' Gebruik `!privacy` als je meer wilt weten over de gegevens die ik verzamel en wat ik daarmee doe. Je kan `!help` typen om de hulppagina te zien.' +
-            ' Misbruik van deze bot of van mensen die je via deze bot ontmoet, wordt niet getolereerd en je kan volledig verbannen worden van het gebruik van deze bot. Je kan `!report gebruiker#1234` typen om een specifieke gebruiker te rapporteren. Veel plezier!'
+        `Hi! Ik ben ${message.client.user?.username} en ik ben hier om jou met een andere persoon te matchen die dezelfde interesses als jij heeft.\nGebruik \`!privacy\` als je meer wilt weten over de gegevens die ik verzamel en wat ik daarmee doe. Je kan \`!help\` typen om de hulppagina te zien.\nMisbruik van deze bot of van mensen die je via deze bot ontmoet, wordt niet getolereerd en je kan volledig verbannen worden van het gebruik van deze bot. Je kan \`!report gebruiker#1234\` typen om een specifieke gebruiker te rapporteren. Veel plezier!`
     );
 
     return await sendQuestion(message, { id: message.author.id, categories: [] });
