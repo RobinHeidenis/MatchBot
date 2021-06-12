ec26f5af560627c4c6052990d17aa25944a56921
Julien <jzapata@live.nl>
2021-06-12 12:14:12 +0200

    feat: require user interaction prior to beginning registration

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -22,10 +22,19 @@ export = {
 
 async function startRegistration(message: Message) {
     const sentMsg = await message.author.send(
-        `Hi! Ik ben ${message.client.user?.username} en ik ben hier om jou met een andere persoon te matchen die dezelfde interesses als jij heeft.\nGebruik \`!privacy\` als je meer wilt weten over de gegevens die ik verzamel en wat ik daarmee doe. Je kan \`!help\` typen om de hulppagina te zien.\nMisbruik van deze bot of van mensen die je via deze bot ontmoet, wordt niet getolereerd en je kan volledig verbannen worden van het gebruik van deze bot. Je kan \`!report gebruiker#1234\` typen om een specifieke gebruiker te rapporteren. Veel plezier!\n\nKlik op het groene vinkje om te beginnen met registeren.`
+        `Hi! Ik ben ${message.client.user?.username} en ik ben hier om jou met een andere persoon te matchen die dezelfde interesses als jij heeft.\nGebruik \`!privacy\` als je meer wilt weten over de gegevens die ik verzamel en wat ik daarmee doe. Je kan \`!help\` typen om de hulppagina te zien.\nMisbruik van deze bot of van mensen die je via deze bot ontmoet, wordt niet getolereerd en je kan volledig verbannen worden van het gebruik van deze bot. Je kan \`!report gebruiker#1234\` typen om een specifieke gebruiker te rapporteren. Veel plezier!\n\nJe krijgt straks ${questions.length} vragen voorgelegd. Zodra je op het groene vinkje klikt, begint de registratie.`
+    );
+    await sentMsg.react('✅');
+    const collector = sentMsg.createReactionCollector(
+        ({ emoji }: MessageReaction, user: User) => user.id === message.author.id && '✅' === emoji.name
     );
 
-    return await sendQuestion(message, { id: message.author.id, categories: [] });
+    collector.on('collect', async (reaction, user) => {
+        console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
+        collector.stop();
+
+        await sendQuestion(message, { id: message.author.id, categories: [] });
+    });
 }
 
 async function sendQuestion(message: Message, userData: UserData, questionNumber = 0) {
