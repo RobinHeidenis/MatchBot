5b645687a4a64eb44efa91baf8354f08ecaf4f22
Julien <jzapata@live.nl>
2021-06-12 11:51:20 +0200

    refactor: check if user exists outside registration flow

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -10,18 +10,19 @@ export = {
     name: 'register',
     description: 'Maak een nieuw profiel aan. Op basis van dit profiel kan ik nieuwe mensen voor je zoeken.',
     execute(message: Message) {
+        if (userExists(message.author)) {
+            return message.author.send(
+                'Je bent al geregistreerd. Gebruik `!match` om te beginnen met nieuwe mensen ontmoeten!'
+            );
+        }
+
         return startRegistration(message);
     },
 };
 
 async function startRegistration(message: Message) {
-    if (userExists(message.author)) {
-        return message.author.send(
-            'Je bent al geregistreerd. Gebruik `!match` om te beginnen met nieuwe mensen ontmoeten!'
-        );
-    }
-    await message.author.send(
-        `Hi! Ik ben ${message.client.user?.username} en ik ben hier om jou met een andere persoon te matchen die dezelfde interesses als jij heeft.\nGebruik \`!privacy\` als je meer wilt weten over de gegevens die ik verzamel en wat ik daarmee doe. Je kan \`!help\` typen om de hulppagina te zien.\nMisbruik van deze bot of van mensen die je via deze bot ontmoet, wordt niet getolereerd en je kan volledig verbannen worden van het gebruik van deze bot. Je kan \`!report gebruiker#1234\` typen om een specifieke gebruiker te rapporteren. Veel plezier!`
+    const sentMsg = await message.author.send(
+        `Hi! Ik ben ${message.client.user?.username} en ik ben hier om jou met een andere persoon te matchen die dezelfde interesses als jij heeft.\nGebruik \`!privacy\` als je meer wilt weten over de gegevens die ik verzamel en wat ik daarmee doe. Je kan \`!help\` typen om de hulppagina te zien.\nMisbruik van deze bot of van mensen die je via deze bot ontmoet, wordt niet getolereerd en je kan volledig verbannen worden van het gebruik van deze bot. Je kan \`!report gebruiker#1234\` typen om een specifieke gebruiker te rapporteren. Veel plezier!\n\nKlik op het groene vinkje om te beginnen met registeren.`
     );
 
     return await sendQuestion(message, { id: message.author.id, categories: [] });
